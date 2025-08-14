a888daeb2b9d8f41a84f5c6e864d76964810fe39
Alexey <a.lshe@yandex.by>
2025-08-14 15:40:21 +0300

    Done tabs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 window.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll(".tabheader__item"),
     tabsContent = document.querySelectorAll(".tabcontent"),
@@ -5,30 +6,29 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function hideTabContent() {
     tabsContent.forEach((item) => {
-      item.style.display = "none";
+      item.classList.add("hide", "fade");
+      item.classList.remove("show", "fade")
     });
     tabs.forEach((item) => {
       item.classList.remove("tabheader__item_active");
     });
   }
-
   function showTabContent(i = 0) {
-    tabsContent[i].style.display = "block";
+    tabsContent[i].classList.add("show", "fade");
     tabs[i].classList.add("tabheader__item_active");
   }
-
   hideTabContent();
   showTabContent();
 
   tabsParent.addEventListener("click", (event) => {
     const target = event.target;
-    if (target && target.classList.contains("tabheader__item")) {
-      tabs.forEach((item, i) => {
+    if (target && target.classList.contains("tabheader__item"))
+      tabs.forEach((item, index) => {
         if (target == item) {
           hideTabContent();
-          showTabContent(i);
+          showTabContent(index);
         }
       });
-    }
+    console.log(target);
   });
 });
