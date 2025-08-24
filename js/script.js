50f11aa0b12fbd3555cac7eb5ee8be34a3bbf032
Alexey <a.lshe@yandex.by>
2025-08-24 12:53:32 +0300

    Creating slider. Option 1

--- js/script.js
+++ js/script.js
@@ -247,7 +247,7 @@ window.addEventListener("DOMContentLoaded", () => {
   //   });
   // }
 
-    //  FORMS
+  //  FORMS
 
   const forms = document.querySelectorAll("form");
   const message = {
@@ -327,7 +327,45 @@ window.addEventListener("DOMContentLoaded", () => {
     }, 4000);
   }
 
-  fetch("http://localhost:3000/menu")
-    .then((data) => data.json())
-    .then((res) => console.log(res));
+  const slides = document.querySelectorAll(".offer__slide");
+  const prevBtn = document.querySelector(".offer__slider-prev");
+  const nextBtn = document.querySelector(".offer__slider-next");
+  const total = document.querySelector("#total");
+  const current = document.querySelector("#current");
+  let slideIndex = 1;
+
+  showSlides(slideIndex);
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+  } else {
+    total.textContent = slides.length;
+  }
+
+  function showSlides(n) {
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    slides.forEach((item) => {
+      item.style.display = "none";
+    });
+    slides[slideIndex - 1].style.display = "block";
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
+
+  }
+
+  function plusSlides(n) {
+    showSlides((slideIndex += n));
+  }
+
+  prevBtn.addEventListener("click", () => plusSlides(-1));
+  nextBtn.addEventListener("click", () => plusSlides(1));
 });
